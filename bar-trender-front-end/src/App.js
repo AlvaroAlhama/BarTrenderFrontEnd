5d8ab138b637c81b0712c02a6d860e17612e0830
CoboJose <cobogue@gmail.com>
2021-02-28 13:53:38 +0100

    INIT FRONT:
    init react front

--- bar-trender-front-end/src/App.js
+++ bar-trender-front-end/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [msg, setMsg] = useState('No connection with API')
+
+  //Exec only once, when the page load
+  useEffect(() => {
+    
+    axios.get('http://127.0.0.1:8000/main/test/')
+      .then(res => {
+        setMsg(res.data.msg)
+      })
+      .catch(error => console.log(error))
+
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{msg}</h2>
     </div>
-  );
+  )
 }
 
 export default App;

d996a599d21e72c75b749684302a1a474d35a4a7
FranQuintela <fraquivel@alum.us.es>
2021-03-12 09:59:46 +0100

    landing_page_initial_commit

---
+++ bar-trender-front-end/src/components/Headers/LandingPageHeader.js
@@ -0,0 +1,43 @@
+import React from "react";
+import bg from '../../assets/img/bg-landing.png';
+
+// reactstrap components
+import { Button, Container } from "reactstrap";
+import 'font-awesome/css/font-awesome.min.css';
+
+// core components
+
+function LandingPageHeader() {
+  let pageHeader = React.createRef();
+
+  React.useEffect(() => {
+    if (window.innerWidth > 991) {
+      const updateScroll = () => {
+        let windowScrollTop = window.pageYOffset / 3;
+        pageHeader.current.style.transform =
+          "translate3d(0," + windowScrollTop + "px,0)";
+      };
+      window.addEventListener("scroll", updateScroll);
+      return function cleanup() {
+        window.removeEventListener("scroll", updateScroll);
+      };
+    }
+  });
+  return (
+    <>
+
+      <div className="page-header page-header-medium">
+        <div
+          className="page-header-image"
+          style={{
+            backgroundImage: `url(${bg})`,
+          }}
+          ref={pageHeader}
+        ></div>
+         
+      </div>
+    </>
+  );
+}
+
+export default LandingPageHeader;
--- bar-trender-front-end/src/index.js
+++ bar-trender-front-end/src/index.js
@@ -1,17 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 import './index.css';
 import App from './App';
 
+// styles for this kit
+import "./assets/css/bootstrap.min.css";
+import "./assets/scss/now-ui-kit.scss?v=1.4.0";
+import "./assets/demo/demo.css?v=1.4.0";
+import "./assets/demo/nucleo-icons-page-styles.css?v=1.4.0";
+// pages for this kit
+import Index from "./views/Index.js";
+import NucleoIcons from "./views/NucleoIcons.js";
+import LoginPage from "./views/examples/LoginPage.js";
+import LandingPage from "./views/examples/LandingPage.js";
+import ProfilePage from "./views/examples/ProfilePage.js";
 
 const app = (
+  
   <React.StrictMode>
 
-    <App/>
-
+    {/* <App/> */}
+    <BrowserRouter>
+    <Switch>
+      <Switch>
+        <Route path="/index" render={(props) => <App {...props} />} />
+        <Route path="/demo-index" render={(props) => <Index {...props} />} />
+        <Route
+          path="/nucleo-icons"
+          render={(props) => <NucleoIcons {...props} />}
+        />
+        <Route
+          path="/landing-page"
+          render={(props) => <LandingPage {...props} />}
+        />
+        <Route
+          path="/profile-page"
+          render={(props) => <ProfilePage {...props} />}
+        />
+        <Route
+          path="/login-page"
+          render={(props) => <LoginPage {...props} />}
+        />
+        <Redirect to="/index" />
+        <Redirect from="/" to="/index" />
+      </Switch>
+    </Switch>
+  </BrowserRouter>
   </React.StrictMode>
 );
 

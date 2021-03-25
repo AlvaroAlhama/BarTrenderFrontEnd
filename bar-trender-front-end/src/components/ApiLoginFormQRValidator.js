84422bc4fa9bdb2649d5dea1e556e910b6e9a2dd
Alejandro Fuentes Gómez <48445978+alefuegom@users.noreply.github.com>
2021-03-25 18:33:57 +0100

    [ADD] LOGIN IN ORDER TO VALIDATE QR
    It's been added the following funcionality:
        - A establishment must be logged in the app in order to verify a discount.
    
    So far, the funcionality of validate a QR is working. There are to fix async call and user id in the API call.

---
+++ bar-trender-front-end/src/components/ApiLoginFormQRValidator.js
@@ -0,0 +1,171 @@
+
+import React from "react";
+import { Redirect } from "react-router-dom";
+
+class POSTLoginFormQRValidator extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+
+      input: {},
+
+      errors: {},
+
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+
+  async handleLogin() {
+    let errors = {};
+    var url = "https://develop-backend-sprint-01.herokuapp.com/v1/authentication/login";
+    // Call to the api with the credentials given by the user
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { apiKey: "apikeytest" },
+      body: JSON.stringify(this.state.input),
+    });
+    if (response.ok) {
+      var r = await response.json();
+      var token = r.token;
+      sessionStorage.setItem("token", token);
+      window.location.href = "/establishmentQrScan";
+    } else {
+      const data = await response.blob();
+      this.setState({ loading: false });
+      errors["email"] = "Email o contraseña incorrecta.";
+    }
+    this.setState({
+      errors: errors,
+    });
+  }
+
+
+  handleChange(event) {
+    let input = this.state.input;
+    input[event.target.name] = event.target.value;
+    this.setState({
+
+      input,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    if (this.validate()) {
+      let errors = {};
+
+      let input = {};
+
+      input["email"] = "";
+
+      input["password"] = "";
+    }
+
+    this.handleLogin(event);
+
+  }
+
+  validate() {
+    let input = this.state.input;
+
+
+    let errors = {};
+
+    let isValid = true;
+
+    if (!input["email"]) {
+      isValid = false;
+
+      errors["email"] = "Escriba una dirección de correo electrónico.";
+    }
+
+    if (typeof input["email"] !== "undefined") {
+      var pattern = new RegExp(
+        /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+      );
+
+      if (!pattern.test(input["email"])) {
+        isValid = false;
+
+        errors["email"] =
+          "Escriba una dirección de correo electrónico correcta.";
+      }
+    }
+
+    if (!input["password"]) {
+      isValid = false;
+
+      errors["password"] = "Escriba una contraseña.";
+    }
+
+    this.setState({
+      errors: errors,
+    });
+
+
+    return isValid;
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div class="form-group my-1">
+
+            <input
+              type="text"
+              name="email"
+
+              value={this.state.input.email}
+              onChange={this.handleChange}
+              class="form-control"
+              placeholder="Correo electrónico"
+              id="email"
+            />
+
+
+            <div className="text-danger">{this.state.errors.email}</div>
+          </div>
+
+          <div class="form-group my-4">
+            <input
+
+              name="password"
+              type="password"
+
+              value={this.state.input.password}
+
+              onChange={this.handleChange}
+              placeholder="Contraseña"
+              class="form-control"
+            />
+
+
+            <div className="text-danger align-center">
+              {this.state.errors.password}
+            </div>
+          </div>
+
+          <div class="text-center">
+            <input
+              type="submit"
+              value="Iniciar sesión"
+              class="btn btn-primary"
+            />
+          </div>
+
+        </form>
+      </div>
+    );
+  }
+}
+
+export default POSTLoginFormQRValidator;
+
--- bar-trender-front-end/src/views/EstablishmentsQrScan.js
+++ bar-trender-front-end/src/views/EstablishmentsQrScan.js
@@ -1,105 +1,134 @@
-import MainNavbar from "components/Navbars/MainNavbar";
 import React, { useEffect, useState } from "react";
+import { Modal, ModalBody } from "reactstrap";
 
-import "./css/EstablishmentQrScan.css"
+import "./css/EstablishmentQrScan.css";
 import barTrender60 from "../assets/img/barTrender60.png";
 import success_boy from "../assets/img/success_scan-min.png";
 import fail_boy from "../assets/img/fail-scan.png";
+import POSTLoginFormQRValidator from "components/ApiLoginFormQRValidator";
 
-function EstablishmentQrScan(){
+function EstablishmentQrScan() {
+  var token = sessionStorage.getItem("token");
+  document.body.style.fontFamily = "Dosis";
+  const [appState, setAppState] = useState({
+    loading: false,
+    error: null,
+  });
 
-var token = sessionStorage.getItem("token");
-document.body.style.fontFamily = "Dosis";
-const [appState, setAppState] = useState({
-  loading: false,
-  error:null
-});
+  useEffect(() => {
+    setAppState({ loading: true });
+    const url =
+      "https://develop-backend-sprint-01.herokuapp.com/v1/establishments/1/discounts/1/scan?client_id=1&redirect_url=www.google.es";
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        token:token,
+      },
+    })
+      .then(
+        (response) => response.json())
+      .then((data) => {
+        setAppState({ loading: false, error: data.error });
+      });
+  }, [setAppState]);
+  const [modal1, setModal1] = React.useState(true);
 
-useEffect(() => {
-setAppState({ loading: true });
-const url = "https://develop-backend-sprint-01.herokuapp.com/v1/establishments/1/discounts/1/scan?client_id=1&redirect_url=www.google.es"
-fetch (url, {
-  method:"GET",
-  headers:{
-    "content-type": "application/json",
-    "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6Im93bmVyMUBnbWFpbC5jb20iLCJyb2wiOiJvd25lciIsImV4cGlyZXNJbiI6MTYxNjY3ODg4NX0.xAyBKGUQAk_jMrrcxIVNo2VHfXZkeMV6L2TfIqr_inQ",
+  if (!token) {
+    return (
+      <Modal isOpen={modal1} toggle={() => setModal1(false)}>
+        <div className="modal-header justify-content-center">
+          <button
+            className="close"
+            type="button"
+            onClick={() => setModal1(false)}
+          >
+            <i className="now-ui-icons ui-1_simple-remove"></i>
+          </button>
+          <h4 className="title title-up">Inicio de Sesión</h4>
+        </div>
+        <div class="container">
+          <hr />
+        </div>
+        <ModalBody>
+          <POSTLoginFormQRValidator />
+        </ModalBody>
+      </Modal>
+    );
+  } else {
+    if (appState.error != null) {
+      document.body.style.backgroundColor = "#c21313";
+      return (
+        <>
+          <div class="container mt-5 pt-5">
+            <div class="row justify-content-center">
+              <img src={barTrender60} class="img-fluid" />
+              <h1 class="my-auto text-white ml-3">BARTRENDER</h1>
+            </div>
+            <div class="row justify-content-center mt-5">
+              <h1 className="text-white text-center font-weight-bold">
+                ¡OOPS! HA OCURRIDO EL SIGUIENTE PROBLEMA
+              </h1>
+            </div>
+            <div class="row justify-content-center   mt-3">
+              <h2 class="my-auto text-white justify-content-center">
+                {appState.error}
+              </h2>
+              <img
+                src={fail_boy}
+                className="img-fluid"
+                style={{
+                  width: "100%",
+                  maxWidth: "18.75em",
+                }}
+              />
+            </div>
+            <div class="row justify-content-center mt-5">
+              <h3 id="index-button-fail">
+                <a href="/index" className="text-white m-4">
+                  Volver a inicio
+                </a>
+              </h3>
+            </div>
+          </div>
+        </>
+      );
+    } else {
+      document.body.style.backgroundColor = "#24a327";
+      return (
+        <>
+          <div class="container mt-5 pt-5">
+            <div class="row justify-content-center">
+              <img src={barTrender60} class="img-fluid" />
+              <h1 class="my-auto text-white ml-3">BARTRENDER</h1>
+            </div>
+            <div class="row justify-content-center mt-5">
+              <h1 className="text-white text-center font-weight-bold">
+                ¡DESCUENTO APLICADO CON ÉXITO!
+              </h1>
+            </div>
+
+            <div class="row justify-content-center mt-3">
+              <img
+                src={success_boy}
+                className="img-fluid"
+                style={{
+                  width: "100%",
+                  maxWidth: "12.5em",
+                }}
+              />
+            </div>
+            <div class="row justify-content-center mt-5">
+              <h3 id="index-button-success">
+                <a href="/index" className="text-white m-4">
+                  Volver a inicio
+                </a>
+              </h3>
+            </div>
+          </div>
+        </>
+      );
+    }
   }
-  }).then(response => response.json())
-    .then(data => {
-      setAppState({loading: false, error: data.error})
-    });
-}, [setAppState]);
-
-if (appState.error != null) {
-  document.body.style.backgroundColor = "#c21313";
-  return (
-    <>
-      <div class="container mt-5 pt-5">
-        <div class="row justify-content-center">
-          <img src={barTrender60} class="img-fluid" />
-          <h1 class="my-auto text-white ml-3">BARTRENDER</h1>
-        </div>
-        <div class="row justify-content-center mt-5">
-          <h1 className="text-white text-center font-weight-bold">
-            ¡OOPS! HA OCURRIDO EL SIGUIENTE PROBLEMA
-          </h1>
-        </div>
-        <div class="row justify-content-center   mt-3">
-          <h2 class="my-auto text-white justify-content-center">{appState.error}</h2>
-          <img
-            src={fail_boy}
-            className="img-fluid"
-            style={{ 
-              width: "100%",
-              maxWidth: "18.75em" }}
-          />
-        </div>
-        <div class="row justify-content-center mt-5">
-          <h3 id="index-button-fail">
-            <a href="/index" className="text-white m-4">
-              Volver a inicio
-            </a>
-          </h3>
-        </div>
-      </div>
-    </>
-  );
-
-} else {
-  document.body.style.backgroundColor = "#24a327";
-  return (
-    <>
-      <div class="container mt-5 pt-5">
-        <div class="row justify-content-center">
-          <img src={barTrender60} class="img-fluid" />
-          <h1 class="my-auto text-white ml-3">BARTRENDER</h1>
-        </div>
-        <div class="row justify-content-center mt-5">
-          <h1 className="text-white text-center font-weight-bold">
-            ¡DESCUENTO APLICADO CON ÉXITO!
-          </h1>
-        </div>
-
-        <div class="row justify-content-center mt-3">
-          <img
-            src={success_boy}
-            className="img-fluid"
-            style={{ 
-              width:"100%",
-              maxWidth: "12.5em" }}
-          />
-        </div>
-        <div class="row justify-content-center mt-5">
-          <h3 id="index-button-success">
-            <a href="/index" className="text-white m-4">
-              Volver a inicio
-            </a>
-          </h3>
-        </div>
-      </div>
-    </>
-  );
-  
-  }
-};
+}
 export default EstablishmentQrScan;

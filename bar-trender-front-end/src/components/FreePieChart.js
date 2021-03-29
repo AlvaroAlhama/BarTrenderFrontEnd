f74555f876a6da2de6305fbba4e3000980de2f89
Fran Quintela <56025446+FranQuintela@users.noreply.github.com>
2021-03-29 18:12:11 +0200

    Stats Functional, no redirect if not logged Css problems

--- bar-trender-front-end/src/components/FreePieChart.js
+++ bar-trender-front-end/src/components/FreePieChart.js
@@ -11,66 +11,42 @@ import {Bar, Line, Pie} from 'react-chartjs-2';
       stats: {},
     });
     
-    constructor() {
-      super();
-  
-      this.state = {
-  
-        input: {},
-  
-        errors: {},
-  
-        modalFail: false,
-        
-        modalSuccess: false,
-  
-      };
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-      this.handleLogin = this.handleLogin.bind(this);
-      this.getDiscountResult = this.getDiscountResult.bind(this);
-    }
-  
-     handleLogin(() => {
-      
-      let errors = {};
-      var url = "http://localhost:8000/v1/authentication/login";
-      // Call to the api with the credentials given by the user
-      const response = await fetch(url, {
-        method: "POST",
-        headers: { apiKey: "8dDc431125634ef43cD13c388e6eCf11" },
-        body: JSON.stringify(this.state.input),
-      });
-      if (response.ok) {
-        var r = await response.json();
-        var token = r.token;
-        sessionStorage.setItem("token", token);
-        this.getDiscountResult();
-      } else {
-        const data = await response.blob();
-        this.setState({ loading: false });
-        errors["email"] = "Email o contraseña incorrecta.";
-      }
-      this.setState({
-        errors: errors,
-      });
-    })
-  
-  
-      
-   
 
+    //  handleLogin(() => {
+      
+    //   let errors = {};
+    //   var url = "http://localhost:8000/v1/authentication/login";
+    //   // Call to the api with the credentials given by the user
+    //   const response = await fetch(url, {
+    //     method: "POST",
+    //     headers: { apiKey: "8dDc431125634ef43cD13c388e6eCf11" },
+    //     body: JSON.stringify(this.state.input),
+    //   });
+    //   if (response.ok) {
+    //     var r = await response.json();
+    //     var token = r.token;
+    //     sessionStorage.setItem("token", token);
+    //     this.getDiscountResult();
+    //   } else {
+    //     const data = await response.blob();
+    //     this.setState({ loading: false });
+    //     errors["email"] = "Email o contraseña incorrecta.";
+    //   }
+    //   this.setState({
+    //     errors: errors,
+    //   });
+    // })
+  
      useEffect(() => {
       var token = sessionStorage.getItem("token");
+      console.log(token,'token');
       const apiUrl = "http://develop-backend-sprint-01.herokuapp.com/v1/stats/get";
       async function loadStats(){
         await fetch(apiUrl, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
-           
-            'token': "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImNsaWVudDFAZ21haWwuY29tIiwicm9sIjoiY2xpZW50IiwiZXhwaXJlc0luIjoxNjE2MzQ0Nzk2fQ.jh4OzXcxV-C9XTlkkWfl99_-4MViTHyjjJHgwKnYg_o",
+            'token': token,
           },
          
           body: JSON.stringify({ filter : 'Bebidas' })

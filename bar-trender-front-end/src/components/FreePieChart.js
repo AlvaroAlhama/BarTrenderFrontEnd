654d9a34665cce7f9f53e5a04cb617727d9c5b07
Fran Quintela <56025446+FranQuintela@users.noreply.github.com>
2021-03-29 22:26:41 +0200

    Solving merce conflicts Solving merce conflicts and handling no data problems

--- bar-trender-front-end/src/components/FreePieChart.js
+++ bar-trender-front-end/src/components/FreePieChart.js
@@ -1,91 +1,114 @@
-import React, { useEffect, useState, Component} from 'react';
-import {Bar, Line, Pie} from 'react-chartjs-2';
+import React, { useEffect, useState, Component } from 'react';
+import { Bar, Line, Pie } from 'react-chartjs-2';
 
 
-  function BeerPieChart(props) {
-    
-    const [modal1, setModal1] = React.useState(false);
-    const [modal2, setModal2] = React.useState(false);
-    const { element } = props;
+function BeerPieChart(props) {
+
+  const [modal1, setModal1] = React.useState(false);
+  const [modal2, setModal2] = React.useState(false);
+  const { element } = props;
 
 
-    
-    const [appState, setAppState] = useState({
-      stats: {},
-    });
-    
 
- 
-  
-     useEffect(() => {
-      var token = sessionStorage.getItem("token");
-      console.log(token,'token');
-      const apiUrl = "http://develop-backend-sprint-01.herokuapp.com/v1/stats/get";
-      async function loadStats(){
-        await fetch(apiUrl, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'token': token,
-          },
-         
-          body: JSON.stringify({ filter : 'Bebidas' })
-        }).then(response => response.json())
-          .then(stats => {
-            setAppState({ stats: stats });
-          });} 
-          loadStats()
-    },[setAppState]);
-  
+  const [appState, setAppState] = useState({
+    stats: {},
+  });
 
+  useEffect(() => {
+    var token = sessionStorage.getItem("token");
+    console.log(token, 'token');
+    const apiUrl = "http://develop-backend-sprint-01.herokuapp.com/v1/stats/get";
+    async function loadStats() {
+      await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'token': token,
+        },
+
+        body: JSON.stringify({ filter: 'Bebidas' })
+      }).then(response => response.json())
+        .then(stats => {
+          setAppState({ stats: stats });
+        });
+    }
+    loadStats()
+  }, [setAppState]);
+
+  console.log(appState.stats, 'appState.stats');
+  if (appState.stats.first != undefined) {
     var graph2 = {
-      chartData:{
+      chartData: {
         labels: [appState.stats.first.name, appState.stats.second.name, appState.stats.third.name, 'Otros'],
-        datasets:[
+        datasets: [
           {
-            label:'Busquedas',
-            data:[
+            label: 'Busquedas',
+            data: [
               appState.stats.first.percentage,
               appState.stats.second.percentage,
               appState.stats.third.percentage,
               appState.stats.other.percentage,
             ],
-            backgroundColor:[
+            backgroundColor: [
               'rgba(255, 99, 132, 0.6)',
               'rgba(54, 162, 235, 0.6)',
               'rgba(255, 206, 86, 0.6)',
               'rgba(75, 192, 192, 0.6)',
             ],
-            
+
           }
         ]
-      
-    }
-  };
 
+      }
+    };
+  } else {
+    var graph2 = {
+      chartData: {
+        labels: ['PLACEHOLDER', 'PLACEHOLDER', 'PLACEHOLDER', 'Otros'],
+        datasets: [
+          {
+            label: 'Busquedas',
+            data: [
+              10,
+              20,
+              30,
+              40,
+            ],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+            ],
 
-  
+          }
+        ]
 
-    return (
-      <div className="chart">
-        <Pie
-          data={graph2.chartData}
-          options={{
-            title:{
-              display:true,
-              text:'¿Qué se busca más?',
-              fontSize:25
-            },
-            legend:{
-              display:true,
-              position:'right'
-            }
-          }}
-        />
-        
-      </div>
-        )
+      }
+    };
   }
+
+
+  return (
+    <div className="chart">
+      <Pie
+        data={graph2.chartData}
+        options={{
+          title: {
+            display: true,
+            text: '¿Qué se busca más?',
+            fontSize: 25
+          },
+          legend: {
+            display: true,
+            position: 'right'
+          }
+        }}
+      />
+
+    </div>
+  )
+}
 
 
 export default BeerPieChart;

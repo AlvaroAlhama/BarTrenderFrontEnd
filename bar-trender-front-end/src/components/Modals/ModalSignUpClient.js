dbcdcf6dd59ce67fa2afa1511668e0906b548b4c
Alejandro Fuentes Gómez <48445978+alefuegom@users.noreply.github.com>
2021-04-08 19:41:24 +0200

    [IMPL] Register Client Form added
    It's been added the form in which a client can sign up in the app. Moreover, I implemented validations in order to register a client properly.
    The next step is to call the api in order to save the client data.

--- bar-trender-front-end/src/components/Modals/ModalSignUpClient.js
+++ bar-trender-front-end/src/components/Modals/ModalSignUpClient.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-
 import { Button, Modal, ModalBody } from "reactstrap";
 
-import POSTLoginForm from "../ApiLoginForm";
+import ApiSignUpClientForm from "../ApiSignUpClientForm";
 
-function ModalLogin() {
+function ModalSignUpClient() {
   const [modal1, setModal1] = React.useState(false);
 
   var token = sessionStorage.getItem("token");
-  if (!token) {
     return (
       <>
         <i
@@ -17,9 +15,10 @@ function ModalLogin() {
           className="mr-1"
           onClick={() => setModal1(true)}
           id="login-tooltip"
-          className="far fa-user fa-lg mt-1"
+          className="fal fa-key w-100 fa-lg mt-1"
         ></i>
-        <p className="d-lg-none d-xl-none">Inicio de Sesión</p>
+
+        <p className="d-lg-none d-xl-none">Regitstro</p>
         <Modal isOpen={modal1} toggle={() => setModal1(false)}>
           <div className="modal-header justify-content-center">
             <button
@@ -29,13 +28,13 @@ function ModalLogin() {
             >
               <i className="now-ui-icons ui-1_simple-remove"></i>
             </button>
-            <h4 className="title title-up">Inicio de Sesión</h4>
+            <h4 className="title title-up">Registro para clientes </h4>
           </div>
           <div class="container">
             <hr />
           </div>
           <ModalBody>
-            <POSTLoginForm />
+            <ApiSignUpClientForm />
             <div class="mt-2 mb-4 text-center">
               <a href="#">¿Aún no tienes una cuenta? Regístrate</a>
             </div>
@@ -43,25 +42,6 @@ function ModalLogin() {
         </Modal>
       </>
     );
-  } else {
-    return (
-      <>
-        <i
-          color="primary"
-          className="mt-1"
-          id="logout-tooltip"
-          onClick={() => {
-            sessionStorage.clear();
-            window.location.href = "/index";
-          }}
-          className="fas fa-power-off fa-lg mt-1"
-          
-        />
-        <p className="d-lg-none d-xl-none">Cerrar sesión</p>
-        
-      </>
-    );
-  }
 }
 
-export default ModalLogin;
+export default ModalSignUpClient;

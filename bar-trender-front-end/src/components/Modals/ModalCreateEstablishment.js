11c39ac39c39e30beb236e5fc66b6c3edec72c7c
Alejandro Fuentes Gómez <48445978+alefuegom@users.noreply.github.com>
2021-04-19 17:52:07 +0200

    [FIX] RESPONSIVE VIEW MAIN MENU

--- bar-trender-front-end/src/components/Modals/ModalCreateEstablishment.js
+++ bar-trender-front-end/src/components/Modals/ModalCreateEstablishment.js
@@ -4,13 +4,26 @@ import { Button, Modal, ModalBody } from "reactstrap";
 
 function ModelCreateEstablishment() {
   const [modal1, setModal1] = React.useState(false);
+  function reportWindowSize() {
+    const { innerWidth: width, innerHeight: height } = window;
+    if(width < 750 && document.getElementById("create-tooltip")!=null ){
+      document.getElementById("create-tooltip").classList.remove("mt-1");
+      document.getElementById("create-tooltip").classList.add("my-auto");
+    }
+    if(width > 750 && document.getElementById("create-tooltip")!=null ){
+      document.getElementById("create-tooltip").classList.remove("my-auto");
+      document.getElementById("create-tooltip").classList.add("mt-1");
+    }
+  }
+  reportWindowSize();
+  window.addEventListener('resize', reportWindowSize);
     return (
       <>
         <i
           color="primary"
           className="mr-1"
           onClick={() => setModal1(true)}
-          id="login-tooltip"
+          id="create-tooltip"
           className="fal fa-plus-square mt-1 text-white fa-lg"
         ></i>
         <p className="d-lg-none text-white d-xl-none ml-2">Crear establecimiento</p>

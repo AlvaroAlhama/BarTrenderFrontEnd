f028c0f9835d4b7e82292f7c1afb121a398f6ee8
Alejandro Fuentes Gómez <48445978+alefuegom@users.noreply.github.com>
2021-04-23 20:12:39 +0200

    [IMPL] Establishment Selected Modal
    It's been changed the visual interface of the Selected Establishment Modal in order to improve the customer experience.

--- bar-trender-front-end/src/components/Modals/ModalSelectedElement.js
+++ bar-trender-front-end/src/components/Modals/ModalSelectedElement.js
@@ -1,83 +1,89 @@
 import React, { useEffect, useState } from "react";
-import {
-  Row,
-  Col,
-} from "reactstrap";
+import { Container, Row, Col, ModalHeader } from "reactstrap";
 
 // reactstrap components
-import {
-  Button,
-  Modal,
-  ModalBody,
-  ModalFooter,
-} from "reactstrap";
+import { Button, Modal, ModalBody, ModalFooter } from "reactstrap";
 
 // core components
-import image_left from '../../assets/img/bar-interior.jpg';
-import Qr from '../../App.js'
+import image_left from "../../assets/img/bar-interior.jpg";
+import Qr from "../../App.js";
 
 //css components
-import '../../views/css/ModalQR.css'
+import "../../views/css/ModalQR.css";
 import ListDiscount from "../ListDiscount";
-import './ModalSelectedElement.css';
+import "./ModalSelectedElement.css";
 
-import Map from "components/Map.js"
-
-
+import Map from "components/Map.js";
 
 function ModalSelectedElement(prop) {
   const [modal1, setModal1] = React.useState(false);
   const [modal2, setModal2] = React.useState(false);
   const { element } = prop;
+  const description = element.desc_text != null;
 
   const [appState, setAppState] = useState({
     discounts: {},
   });
   const ubicacion = {
     lat: 36.92043226009566,
-    lng: -6.080399144405965
+    lng: -6.080399144405965,
   };
-  const location = element.street + ", " + element.number + ", " + element.zone + ", " + element.locality;
-
+  const location =
+    element.street +
+    ", " +
+    element.number +
+    ", " +
+    element.zone +
+    ", " +
+    element.locality;
 
   async function loadDiscounts() {
-    const apiUrl = "https://develop-backend-sprint-01.herokuapp.com/v1/establishments/" + element.id + "/discounts/get?page=1&all=False";
+    const apiUrl =
+      "https://develop-backend-sprint-01.herokuapp.com/v1/establishments/" +
+      element.id +
+      "/discounts/get?page=1&all=False";
     await fetch(apiUrl, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
-        'token': '',
-        'apiKey': '8dDc431125634ef43cD13c388e6eCf11',
-      }
-    }).then(response => response.json())
-      .then(discounts => {
+        "Content-Type": "application/json",
+        token: "",
+        apiKey: "8dDc431125634ef43cD13c388e6eCf11",
+      },
+    })
+      .then((response) => response.json())
+      .then((discounts) => {
         setAppState({ discounts: discounts });
       });
+    console.log("ELEMENTO");
+    console.log(element);
   }
-
 
   return (
     <>
-
       <div class="card h-auto bg-white border border-dark">
         <div class="card-body card-img-top">
           <img
             className=""
-            src={element.image != null ? element.image: image_left}
-            onClick={() => { setModal1(true); loadDiscounts(); }}
+            src={element.image != null ? element.image : image_left}
+            onClick={() => {
+              setModal1(true);
+              loadDiscounts();
+            }}
             alt=""
           />
-
         </div>
         <div class="card-footer">
-          <p class="card-title text-center">
-            {element.name}
-          </p>
-
+          <p class="card-title text-center">{element.name}</p>
         </div>
       </div>
 
-      <Modal animation={false} size="lg" modalClassName="modal-info" isOpen={modal1} toggle={() => setModal1(false)}>
+      <Modal
+        animation={false}
+        size="lg"
+        modalClassName="modal-info"
+        isOpen={modal1}
+        toggle={() => setModal1(false)}
+      >
         <div className="modal-header justify-content-center">
           <button
             className="close"
@@ -88,42 +94,87 @@ function ModalSelectedElement(prop) {
           </button>
           <h1 className="title text-center title-up my-auto">{element.name}</h1>
 
+          <ModalHeader>
+            <h2 className="text-center">{element.name_text}</h2>
+          </ModalHeader>
         </div>
         <ModalBody className="text-white">
-          <img
-            className="image-container img-fluid image-left mb-0"
-            src={element.image != null ? element.image: image_left}
-            onClick={() => setModal1(true)}
-            alt=""
-          />
-          <h3 className="text-center mt-3">Información del establecimiento</h3>
-          <p>
-            Nombre del establecimiento: {element.name}
-          </p>
-
-          <p>{element.phone}</p>
-          <p>{element.zone}</p>
+          <Row className="justify-content-center">
+            <img
+              className="image-container img-fluid mb-0 w-75"
+              src={element.image != null ? element.image : image_left}
+              onClick={() => setModal1(true)}
+              alt=""
+            />
+          </Row>
+          <Container>
+            <Row className="my-3">
+              <Col className="my-3" lg="6" md="6" xs="12">
+                <Container className="mx-auto">
+                  <p>
+                    <i class="fal fa-phone-alt text-primary"></i>{" "}
+                    {element.phone_number}
+                  </p>
+                  <p>
+                    <i class="fal fa-university text-primary"></i>{" "}
+                    {element.locality_text}
+                  </p>
+                  <p>
+                    <i class="fal fa-map-signs text-primary"></i>{" "}
+                    {element.street_text}
+                  </p>
+                </Container>
+              </Col>
+              <Col className="my-3" lg="6" md="6" xs="12">
+                <Row className="justify-content-center mb-2">
+                  <i class="my-auto fal fa-tags mr-2 text-primary"></i>
+                  <p className=" my-auto text-primary">Etiquetas</p>
+                </Row>
+                <Row>
+                  {element.tags.map((tag) => {
+                    return (
+                      <Col className="my-1" lg="4" md="4" xs="4">
+                        <div className="text-center">
+                          <p className="text-white">{tag.name}</p>
+                        </div>
+                      </Col>
+                    );
+                  })}
+                </Row>
+              </Col>
+            </Row>
+            <Container>
+              <Row className="justify-content-center">
+                <i class="fal fa-quote-right my-auto mr-2 text-primary"></i>
+                <p className="my-auto text-primary">
+                  Descripción del establecimiento
+                </p>
+              </Row>
+              <Row className="justify-content-center my-2">
+                {description && <p>{element.desc_text}</p>}
+                {!description && <p>Un establecimiento de BarTrender</p>}
+              </Row>
+            </Container>
+          </Container>
+          <Row className="justify-content-center"></Row>
           <ListDiscount discounts={appState.discounts} />
 
           <h3>Ubicacion</h3>
           <Map location={location} />
-
-
         </ModalBody>
         <div className="modal-footer">
-
-
-          <Button
-            color="danger"
-            type="button"
-            onClick={() => setModal1(false)}
-          >
+          <Button color="danger" type="button" onClick={() => setModal1(false)}>
             Cerrar
-                    </Button>
+          </Button>
         </div>
       </Modal>
 
-      <Modal className="modal-qr" centered="true" isOpen={modal2} toggle={() => setModal2(false)}>
+      <Modal
+        className="modal-qr"
+        centered="true"
+        isOpen={modal2}
+        toggle={() => setModal2(false)}
+      >
         <div className="modal-header justify-content-center">
           <button
             className="close"

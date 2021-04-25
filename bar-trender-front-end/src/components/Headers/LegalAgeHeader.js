8f5ccf94951624dd5ba1270611dc0ad9992ec951
j0ysuru <carlosk1llj0yp@gmail.com>
2021-04-25 19:37:41 +0200

    main view +18 advice

---
+++ bar-trender-front-end/src/components/Headers/LegalAgeHeader.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+// reactstrap components
+import 'font-awesome/css/font-awesome.min.css';
+import './LandingPageHeader.css';
+import video from "../../assets/videos/Beer.gif";
+import { Button } from "bootstrap";
+
+// core components
+
+function LegalAgeHeader() {
+    var legalAge = "";
+
+  return (
+    <>
+    <header>
+    <div class="overlay">
+    <img className="beer" alt="" src={video}/> 
+    </div>
+    
+   
+      <div class="container d-flex h-100 text-center align-items-center">
+        <div class="w-100 text-white">
+          <h1 class="display-3">Para hacer uso de esta aplicación necesita ser mayor de 18 años</h1>
+          <button type="button" class="mx-5 btn btn-primary" onClick={() => window.location.href = "/main",
+           legalAge = "+18", sessionStorage.setItem("legalAge",legalAge)}>Soy mayor de edad</button>
+
+          <button type="button" class="mx-5 btn btn-primary" onClick={() => window.location.href = "/illegal", 
+          legalAge = "-18", sessionStorage.setItem("legalAge",legalAge)}>Soy menor de edad</button>
+        </div>
+      </div>
+ 
+  </header>
+    </>
+  );
+}
+
+export default LegalAgeHeader;
--- bar-trender-front-end/src/views/MainView.js
+++ bar-trender-front-end/src/views/MainView.js
@@ -6,10 +6,19 @@ import React from 'react';
 // core components
 import MainNavbar from "../components/Navbars/MainNavbar.js";
 import LandingPageHeader from "../components/Headers/LandingPageHeader.js";
+import LegalAgeHeader from 'components/Headers/LegalAgeHeader.js';
 
 function LandingPage() {
-
+  
+  var legalAge = sessionStorage.getItem("legalAge");
   // Consuming REST GET
+  if(!legalAge){
+    return(
+      <>
+      <LegalAgeHeader/>
+      </>
+    );
+  }
   return (
     <>
       <MainNavbar />

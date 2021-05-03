9eaf8647ece1b3d497a0ac1c80d16ecd3616719f
Carlos Doblado Herrero <56033933+cardobherUS@users.noreply.github.com>
2021-05-03 20:17:18 +0200

    Update App.js

--- bar-trender-front-end/src/App.js
+++ bar-trender-front-end/src/App.js
@@ -22,7 +22,7 @@ export default class App extends React.Component {
     if(!token){
       this.setState({error: "Necesitas haber iniciado sesión para poder ver el descuento"})
     }else{
-      const url = url_dev+'establishments/'+id_establishment+'/discounts/'+id_discount+'/getQR?custom_host=bartrender-develop-sprint-03.netlify.app';
+      const url = url_dev+'establishments/'+id_establishment+'/discounts/'+id_discount+'/getQR?custom_host=bartrender-sprint-03.netlify.app';
    
 
       const response = await fetch(url, {

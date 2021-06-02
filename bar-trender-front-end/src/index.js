5188475b789f7af62ee4884b7b98fb0323c1ba2a
Jose Cobo <cobogue@gmail.com>
2021-06-02 18:19:42 +0200

    Update index.js

--- bar-trender-front-end/src/index.js
+++ bar-trender-front-end/src/index.js
@@ -36,7 +36,7 @@ import EstablishmentView from "./views/EstablishmentView.js"
 import TermsUse from './views/TermsUse';
 import PrivacyPolicy from './views/PrivacyPolicy';
 
-
+console.log("EY")
 const app = (
   
     <BrowserRouter>
